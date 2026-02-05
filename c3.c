d790660691f6e67dcff3844ca202d0b6182d874d
Shreena Patel <shreenap425@gmail.com>
2026-02-05 10:55:35 +0530

    Add files via upload

---
+++ c3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+      float a=5,b=23.5;
+      float add=a+b;
+      float sub=a-b;
+      float mul=a*b;
+      float div=a/b;
+
+
+      printf("\n add of variable=%f",c);a+b;
+      printf("\n sub of variable=%f",c);a-b;
+      printf("\n mul of variable=%f",c);a*b;
+      printf("\n div of variable=%f",c);a/b;
+
+
+      return 0;
+
+}
