3d0b9b091ed3d129b5a6e6ce598214994e68c885
Shreena Patel <shreenap425@gmail.com>
2026-02-05 10:55:14 +0530

    Add files via upload

---
+++ firstprogramc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the value of a and b");
+    scanf("%d %d",&a,&b);
+    printf("value of a=%d, b=%d",a,b);
+
+
+    return 0;
+
+
+
+
+
+}
