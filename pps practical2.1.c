fcbfd93b652a026c16bf09042f04bdc06ffcf170
Shreena Patel <shreenap425@gmail.com>
2026-02-09 20:02:35 +0530

    Add files via upload

---
+++ pps practical2.1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter three number:");
+    scanf("%d %d %d",&a,&b,&c);
+
+    if (a==b && b==c)
+    {
+      printf("All three numbers are equal: %d\n",a);
+    }
+
+    else if(a>=b && a>=c)
+    {
+      printf("Maximum number is:%d\n",a);
+    }
+
+
+    else if(b>=a && b>=c)
+     {
+     printf("Maximum nubmer is:%d\n",b);
+     }
+
+
+     else
+     {
+      printf("Maximum number is:%d\n",c);
+     }
+     return 0;
+}
